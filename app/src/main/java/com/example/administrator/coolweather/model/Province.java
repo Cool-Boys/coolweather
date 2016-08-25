8e4bc62ae4e2d1eef8068387a77485bc79d7a257
yxl <329991137@qq.com>
2016-08-25 17:31:53 +0800

    颠三倒四
    Signed-off-by: yxl <329991137@qq.com>

---
+++ app/src/main/java/com/example/administrator/coolweather/model/Province.java
@@ -0,0 +1,36 @@
+package com.example.administrator.coolweather.model;
+
+/**
+ * Created by Administrator on 2016-08-25.
+ */
+public class Province {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    private String provinceName;
+    private String provinceCode;
+
+}
